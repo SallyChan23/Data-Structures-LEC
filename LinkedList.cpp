bf340d96972218bdbe92c61e1db013a1c50dab69
SallyChan23 <jeselyntan01@gmail.com>
2023-02-21 10:25:22 +0700

    21-02-2023

---
+++ LinkedList.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+// single linked list
+struct tnode{
+    int value;
+    struct node *next; // agar ke selanjutnya (seperti gerbong kereta, jadi ke gerbong selanjutnya di kereta)
+};
+struct tnode *head = 0; // 0 dengan null adalah sama
+struct tnode *node = (struct tnode*) malloc (sizeof(struct tnode));
+// node -> value = x;
+// node -> next = head;
+// head = node;
+
+// PUSH (insert)
+//1. Push head (depan)
+// 2. push tail (di belakang)
+// 3. push middle (selipin di tengah)
+int main(){
+
+    return 0;
+}
