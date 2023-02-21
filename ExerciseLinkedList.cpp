e7438dda0bc2c443da025aef8f360c0d0aa1b737
SallyChan <jeselyntan01@gmail.com>
2023-02-21 12:41:43 +0700

    OK

--- ExerciseLinkedList.cpp
+++ ExerciseLinkedList.cpp
@@ -8,6 +8,11 @@ struct data{
     struct data *next;
 }*head = NULL, *tail = NULL;
 
+// 1. reserve
+// 2. inisialisasi
+// 3. connect
+
+
 // untuk reserve, inisialisasi, connect
 struct data* newNode(char nama[], int umur){
     // reserve
@@ -40,18 +45,76 @@ void pushHead(char nama[], int umur){
     }
 }
 
+void pushTail(char nama[], int umur){
+    struct data *node = newNode(nama, umur);
+    if(head == NULL){
+        head = tail = NULL;
+    }else{
+        tail -> next = node;
+        tail = node;
+    }
+}
 
+// delete (pop)
+// pop head
+// pop middle
+// pop tail
 
-// 1. reserve
-// 2. inisialisasi
-// 3. connect
+void popHead(){
+    if(head){
+        struct data *curr = head;
+        if (head == tail){
+            head = tail = NULL;
+        }else{
+            head = head -> next;
+        }
+        free(curr);
+    }else{
+        printf("Tidak ditemukan\n");
+    }
+}
+
+void popTail(){
+    if(head){
+        struct data *curr = head;
+        if(head == tail){
+            head = tail = NULL;
+        }else{
+            while(curr -> next != tail)
+                curr = curr -> next;
+            tail = curr;
+            curr = curr -> next;
+            tail -> next = NULL;
+        }
+        free(curr);
+    }else  
+        puts("Data not found");
+}
+
 
 int main(){
-    printf("Hello World");
     pushHead("Andi", 25);
     pushHead("Budi", 18);
     pushHead("Charlie", 15);
     pushHead("Eko", 15);
+    pushTail("Binu", 20);
+    pushTail("Zara", 90);
+    printData();
+
+    puts("--------------");
+    popHead();
+    printData();
+
+    puts("--------------");
+    popHead();
+    printData();
+
+    puts("--------------");
+    popTail();
+    printData();
+
+    puts("---------------");
+    popTail();
     printData();
     return 0;
 }
